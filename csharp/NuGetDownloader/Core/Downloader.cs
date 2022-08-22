f1e56d7264644894b84c6546ad7d25b73507b6ae
aromaa <me@joniaromaa.fi>
2022-08-22 15:48:11 +0300

    Support .NET 5 & 6 in one image

--- csharp/NuGetDownloader/Core/Downloader.cs
+++ csharp/NuGetDownloader/Core/Downloader.cs
@@ -1,112 +1,94 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Collections.Immutable;
+using System.Threading.Tasks.Dataflow;
 using NuGet.Common;
 using NuGet.PackageManagement;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
 using NuGet.Protocol.Core.Types;
 
-namespace NuGetDownloader.Core
+namespace NuGetDownloader.Core;
+
+internal sealed class Downloader
 {
-    internal sealed class Downloader
-    {
-        private ConcurrentDictionary<PackageIdentity, bool> Installed;
-        private ConcurrentQueue<PackageIdentity> DownloadQueue;
+	private readonly ActionBlock<PackageIdentity> downloadQueue;
+	private readonly HashSet<PackageIdentity> downloadQueued;
 
-        private PackageDownloadContext DownloadCache;
+	private readonly PackageDownloadContext downloadCache;
 
-        public string DownloadDirectory { get; }
+	private int downloadCount;
 
-        public ImmutableList<SourceRepository> Sources { get; }
+	public ImmutableList<SourceRepository> Sources { get; }
 
-        internal Downloader(string downloadDirectory, ImmutableList<SourceRepository> sources)
-        {
-            this.Installed = new ConcurrentDictionary<PackageIdentity, bool>();
-            this.DownloadQueue = new ConcurrentQueue<PackageIdentity>();
+	public string DownloadDirectory { get; }
 
-            this.DownloadCache = new PackageDownloadContext(NullSourceCacheContext.Instance);
+	internal Downloader(ImmutableList<SourceRepository> sources, DirectoryInfo outputDir)
+	{
+		this.downloadQueue = new ActionBlock<PackageIdentity>(this.ConsumeItemAsync, new ExecutionDataflowBlockOptions
+		{
+			MaxDegreeOfParallelism = Environment.ProcessorCount
+		});
+		this.downloadQueued = new HashSet<PackageIdentity>();
 
-            this.DownloadDirectory = downloadDirectory;
+		this.downloadCache = new PackageDownloadContext(NullSourceCacheContext.Instance);
 
-            this.Sources = sources;
-        }
+		this.Sources = sources;
 
-        public void TryQueue(PackageIdentity package)
-        {
-            //If we are unaware of the new package and we haven't installed it yet
-            if (this.Installed.TryAdd(package, false))
-            {
-                this.DownloadQueue.Enqueue(package);
-            }
-        }
+		this.DownloadDirectory = outputDir.FullName;
+	}
 
-        public async Task ConsumeQueueAsync()
-        {
-            List<Task> tasks = new List<Task>(Environment.ProcessorCount * 2);
-            while (this.DownloadQueue.Count > 0 || tasks.Count > 0)
-            {
-                while (this.DownloadQueue.TryDequeue(out PackageIdentity? package))
-                {
-                    //If we haven't installed it yet then process it
-                    if (!this.Installed.TryUpdate(package, true, false))
-                    {
-                        continue;
-                    }
+	public Task Completion => this.downloadQueue.Completion;
 
-                    Task task = Task.Run(async () => await this.ConsumeItemAsync(package));
+	public void TryQueue(PackageIdentity package)
+	{
+		lock (this.downloadQueued)
+		{
+			if (!this.downloadQueued.Add(package))
+			{
+				return;
+			}
+		}
 
-                    tasks.Add(task);
+		this.downloadQueue.Post(package);
+	}
 
-                    if (tasks.Count == tasks.Capacity)
-                    {
-                        break;
-                    }
-                }
+	private async Task ConsumeItemAsync(PackageIdentity package)
+	{
+		Console.WriteLine($"Processing {package}");
 
-                Task completedTask = await Task.WhenAny(tasks);
+		DownloadResourceResult result = await PackageDownloader.GetDownloadResourceResultAsync(this.Sources, package, this.downloadCache, this.DownloadDirectory, DebugLogger.instance, CancellationToken.None);
 
-                tasks.Remove(completedTask);
-            }
-        }
+		foreach (PackageDependencyGroup dependencyGroup in await result.PackageReader.GetPackageDependenciesAsync(CancellationToken.None))
+		{
+			foreach (PackageDependency dependency in dependencyGroup.Packages)
+			{
+				PackageIdentity dependencyPackage = new(dependency.Id, dependency.VersionRange.MaxVersion ?? dependency.VersionRange.MinVersion);
 
-        private async Task ConsumeItemAsync(PackageIdentity package)
-        {
-            Console.WriteLine($"Processing {package}");
+				this.TryQueue(dependencyPackage);
+			}
+		}
 
-            DownloadResourceResult result = await PackageDownloader.GetDownloadResourceResultAsync(this.Sources, package, this.DownloadCache, this.DownloadDirectory, DebugLogger.Instance, CancellationToken.None);
+		int count = Interlocked.Increment(ref this.downloadCount);
+		if (count == this.downloadQueued.Count)
+		{
+			this.downloadQueue.Complete();
+		}
+	}
 
-            foreach (PackageDependencyGroup dependencyGroup in await result.PackageReader.GetPackageDependenciesAsync(CancellationToken.None))
-            {
-                foreach (PackageDependency dependency in dependencyGroup.Packages)
-                {
-                    PackageIdentity dependencyPackage = new PackageIdentity(dependency.Id, dependency.VersionRange.MaxVersion ?? dependency.VersionRange.MinVersion);
+	private sealed class DebugLogger : LoggerBase
+	{
+		internal static readonly DebugLogger instance = new();
 
-                    this.TryQueue(dependencyPackage);
-                }
-            }
-        }
+		private DebugLogger() : base(LogLevel.Debug)
+		{
+		}
 
-        private sealed class DebugLogger : LoggerBase
-        {
-            internal static readonly DebugLogger Instance = new DebugLogger();
+		public override void Log(ILogMessage message) => Console.WriteLine(message);
 
-            private DebugLogger() : base(LogLevel.Debug)
-            {
+		public override Task LogAsync(ILogMessage message)
+		{
+			this.Log(message);
 
-            }
-
-            public override void Log(ILogMessage message) => Console.WriteLine(message);
-
-            public override Task LogAsync(ILogMessage message)
-            {
-                this.Log(message);
-
-                return Task.CompletedTask;
-            }
-        }
-    }
+			return Task.CompletedTask;
+		}
+	}
 }
--- csharp/NuGetDownloader/Program.cs
+++ csharp/NuGetDownloader/Program.cs
@@ -1,98 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.CommandLine;
-using System.CommandLine.Invocation;
-using System.IO;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
 using NuGet.Configuration;
 using NuGet.Packaging.Core;
 using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
 using NuGetDownloader.Core;
 
-namespace NuGetDownloader
+namespace NuGetDownloader;
+
+public static class Program
 {
-    public static class Program
-    {
-        public const string DefaultDownloadDir = "packages";
+	public static async Task<int> Main(string[] args)
+	{
+		Option<FileInfo> configFile = new Option<FileInfo>(
+			aliases: new[]
+			{
+				"--config-file",
+				"-c",
+			},
+			description: "The location of the config file.",
+			getDefaultValue: () => new FileInfo("config.json")
+		).ExistingOnly();
 
-        public static async Task Main(string[] args)
-        {
-            RootCommand rootCommand = Program.GenerateCommands();
-            rootCommand.Handler = CommandHandler.Create(async (FileInfo? configFile, DirectoryInfo? outputDir) =>
-            {
-                Downloader downloader = Program.BuildDownloader(outputDir?.FullName ?? Path.Combine(Environment.CurrentDirectory, Program.DefaultDownloadDir));
+		Option<DirectoryInfo> outputDirectory = new Option<DirectoryInfo>(
+			aliases: new[]
+			{
+				"--output-dir",
+				"-o"
+			},
+			description: "The directory to store the downloaded NuGet packages to.",
+			getDefaultValue: () => new DirectoryInfo("packages")
+		).LegalFilePathsOnly();
 
-                await Program.ReadConfig(downloader, configFile?.FullName ?? "config.json");
+		RootCommand rootCommand = new(description: "Command line tool to download selected NuGet packages and all of their dependencies.")
+		{
+			configFile,
+			outputDirectory
+		};
 
-                await downloader.ConsumeQueueAsync();
-            });
+		rootCommand.SetHandler(CommandHandler, configFile, outputDirectory);
 
-            await rootCommand.InvokeAsync(args);
-        }
+		return await rootCommand.InvokeAsync(args);
+	}
 
-        private static RootCommand GenerateCommands()
-        {
-            return new RootCommand(description: "NuGet helper for downloading selected packages and all of their dependencies.")
-            {
-                new Option<FileInfo>(aliases: new[]
-                {
-                    "--config-file",
-                    "-c",
-                }, description: "Config file location.")
-                {
-                    Argument = new Argument<FileInfo>().ExistingOnly()
-                },
+	private static async Task CommandHandler(FileInfo configFile, DirectoryInfo outputDir)
+	{
+		Downloader downloader = new(Program.GetSourceRepositories(), outputDir);
 
-                new Option<DirectoryInfo>(aliases: new[]
-                {
-                    "--output-dir",
-                    "-o"
-                }, description: "Sets the directory where the NuGet packages are downloaded to.")
-                {
-                    Argument = new Argument<DirectoryInfo>()
-                }
-            };
-        }
+		await Program.ReadConfig(downloader, configFile);
 
-        private static Downloader BuildDownloader(string downloadDir)
-        {
-            ImmutableList<SourceRepository> repositories = Program.GetSourceRepositories();
+		await downloader.Completion;
+	}
 
-            Downloader downloader = new Downloader(downloadDir, repositories);
+	private static ImmutableList<SourceRepository> GetSourceRepositories()
+	{
+		ISettings settings = Settings.LoadDefaultSettings(Environment.CurrentDirectory);
 
-            return downloader;
-        }
+		PackageSourceProvider source = new(settings);
+		SourceRepositoryProvider provider = new(source, Repository.Provider.GetCoreV3());
 
-        private static ImmutableList<SourceRepository> GetSourceRepositories()
-        {
-            ISettings settings = Settings.LoadDefaultSettings(Environment.CurrentDirectory);
+		IEnumerable<PackageSource> sources = source.LoadPackageSources();
+		IEnumerable<SourceRepository> repositories = sources.Select(provider.CreateRepository);
 
-            PackageSourceProvider source = new PackageSourceProvider(settings);
-            SourceRepositoryProvider provider = new SourceRepositoryProvider(source, Repository.Provider.GetCoreV3());
+		return repositories.ToImmutableList();
+	}
 
-            IEnumerable<PackageSource> sources = PackageSourceProvider.LoadPackageSources(settings);
-            IEnumerable<SourceRepository> repositories = sources.Select(provider.CreateRepository);
+	private static async Task ReadConfig(Downloader downloader, FileInfo configFile)
+	{
+		await using FileStream stream = configFile.OpenRead();
 
-            return repositories.ToImmutableList();
-        }
+		Dictionary<string, HashSet<string>>? config = await JsonSerializer.DeserializeAsync<Dictionary<string, HashSet<string>>>(stream);
+		if (config is null)
+		{
+			return;
+		}
 
-        private static async Task ReadConfig(Downloader downloader, string configLocation)
-        {
-            Dictionary<string, HashSet<string>> config = JsonSerializer.Deserialize<Dictionary<string, HashSet<string>>>(await File.ReadAllTextAsync(configLocation))!;
+		foreach ((string packageId, HashSet<string> packageVersions) in config)
+		{
+			foreach (string packageVersion in packageVersions)
+			{
+				PackageIdentity package = new(packageId, NuGetVersion.Parse(packageVersion));
 
-            foreach ((string packageId, HashSet<string> packageVersions) in config)
-            {
-                foreach (string packageVersion in packageVersions)
-                {
-                    PackageIdentity package = new PackageIdentity(packageId, NuGetVersion.Parse(packageVersion));
-
-                    downloader.TryQueue(package);
-                }
-            }
-        }
-    }
+				downloader.TryQueue(package);
+			}
+		}
+	}
 }
