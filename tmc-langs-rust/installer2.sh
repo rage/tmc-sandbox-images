e03c980e6d0b65f4f03de4e6929deee86ee7222c
Heliozoa <daniel.x.martinez@helsinki.fi>
2023-01-30 15:34:24 +0200

    Update R scripts in rust to match r

--- tmc-langs-rust/installer2.sh
+++ tmc-langs-rust/installer2.sh
@@ -1,20 +1,7 @@
 #
 echo "--------------------------------------"                               &&
-echo "Installing R 4.2.1                    "                               &&
+echo "Installing R 4.2.1 (done already)     "                               &&
 echo "--------------------------------------"                               &&
-#
-# download R 4.2.1
-cd /rbuild                                                                  &&
-wget https://cran.r-project.org/src/base/R-4/R-4.2.1.tar.gz                 &&
-tar -xzvf R-4.2.1.tar.gz                                                    &&
-cd R-4.2.1                                                                  &&
-# configure, build and install R 4.2.1
-./configure                                                                 &&
-make                                                                        &&
-make install                                                                &&
-cd /rbuild                                                                  &&
-# clean up after building
-rm -rf R-4.2.1                                                              &&
 # install tmcRtestrunner and required libraries
 #
 echo "--------------------------------------"                               &&
